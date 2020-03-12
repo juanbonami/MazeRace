1db393450651384ce649d5851f68166722867cd5
Student_M17 <student_m17@ps-dal-m17.home>
2020-03-11 23:55:38 -0500

    updating git files

---
+++ app.js
@@ -0,0 +1,5 @@
+let LEFT_KEY = 37;
+let UP_KEY = 38;
+let RIGHT_KEY = 39;
+let DOWN_KEY = 40;
+
---
+++ script.js
@@ -0,0 +1,8 @@
+create boilerplate for HTML
+create js file, link the two 
+create 2 divs for the user to manipulate 
+add css to them
+create canvas for the users to interact in
+create components for the user to manipulate 
+begin with the layout of the maze
+
