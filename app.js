0f20a3593f8be6244cd1607111de8275e88988af
Student_M17 <student_m17@ps-dal-m17.home>
2020-03-12 23:55:34 -0500

    set up canvas

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ let UP_KEY = 38;
 let RIGHT_KEY = 39;
 let DOWN_KEY = 40;
 
+let canvas = document.querySelector('canvas');
+console.log(canvas);
+
+let c = canvas.getContext('2d');
+// c.fillRect(100, 100, 100, 100);
+
+// line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = 'blue';
+c.stroke();
