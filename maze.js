4e75ce62b7f4e6e064cd4b00ce5e30062b7323cc
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-16 03:19:13 -0500

    sets the draw function to initialized the maze generator

--- maze.js
+++ maze.js
@@ -30,3 +30,40 @@ function setup() {
   // sets currentCell to the first index of the grid[] array (0,0)
   currentCell = grid[0];
 }
+
+function draw() {
+  background(51);
+  // loops through the length of the grid array
+  for (let i = 0; i < grid.length; i++) {
+    // each iteration calls the show() function
+    grid[i].show();
+  }
+  // current cell is visited
+  currentCell.visited = true;
+  currentCell.highlight();
+
+  frameRate(15);
+  // https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker
+  // STEP 1
+  // next is assigned the value of current cell w function call
+  let next = currentCell.checkNeighbors();
+  if (next) {
+    next.visited = true;
+
+    // STEP 2
+    // push current cell to the stack array
+    backTrack.push(currentCell);
+
+    // STEP 3
+    // removes wall in between currentCell and next cell
+    removeWalls(currentCell, next);
+    // console.log(currentCell, next);
+
+    // STEP 4
+    currentCell = next;
+    // if the stack array is not empty grab a cell from the stack and make it the current
+  } else if (backTrack.length > 0) {
+    // if checkNeibors returns undifined, pop() a cell from the stack array and make it the current cell. Repeats process if undifined is returned.
+    currentCell = backTrack.pop();
+  }
+}
